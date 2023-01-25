4fdaa929f162aa8052dfd992bcefa88bce314262
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-01-25 19:29:52 +0600

    making some progress

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      title: "This is my first Vue App guys!",
+    };
+  },
+};
 </script>
 
 <style>
