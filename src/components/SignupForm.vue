4c7859aec7d4aca56398572e0a6f4c3565742d99
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-05 13:01:16 +0600

    keyboard events modifier

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -11,6 +11,12 @@
       <option value="designer">Web Designer</option>
     </select>
 
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      {{ skill }}
+    </div>
+
     <div class="terms">
       <input type="checkbox" required v-model="checked" />
       <label for="terms">I agree to the terms and conditions</label>
@@ -31,7 +37,20 @@ export default {
       password: "",
       role: "designer",
       checked: false,
+      tempSkill: "",
+      skills: [],
     };
+  },
+
+  methods: {
+    addSkill(e) {
+      if (e.key === "Enter" && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = "";
+      }
+    },
   },
 };
 </script>
