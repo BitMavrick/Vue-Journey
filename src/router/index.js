73ad538d2cb12dee3e93ef6a8e05971ca08a1cea
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-25 11:21:02 +0600

    Folder Sturcture

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
+import JobsView from "../views/jobs/JobsView.vue";
 
 const routes = [
   {
@@ -16,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: "/jobs",
+    name: "jobs",
+    component: JobsView,
   },
 ];
 
---
+++ src/views/jobs/JobsView.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{ job.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        { title: "UX Designer", id: 1, details: "lorem" },
+        { title: "Wev Designer", id: 2, details: "lorem" },
+        { title: "Vue Designer", id: 3, details: "lorem" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
