6b28a76d76e94f109e08dfc1c7da4fbab429cade
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-25 11:34:26 +0600

    Route Parameters

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import JobsView from "../views/jobs/JobsView.vue";
+import JobDetailsView from "../views/jobs/JobDetailsView.vue";
 
 const routes = [
   {
@@ -22,6 +23,11 @@ const routes = [
     path: "/jobs",
     name: "jobs",
     component: JobsView,
+  },
+  {
+    path: "/jobs/:id",
+    name: "JobDetails",
+    component: JobDetailsView,
   },
 ];
 
