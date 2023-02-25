70bfa5960497499d6b2005068443ff3a427c2722
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-25 09:28:32 +0600

    Router Link

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,
+      quis dolores sunt saepe ea illum commodi vitae error molestias voluptas
+      porro nesciunt adipisci atque nemo tenetur sequi ad dicta deleniti
+      repellat ab animi? Praesentium quo rerum ipsa fugiat cumque, veniam natus
+      mollitia est consequatur ad odio dignissimos repellendus aliquid minima
+      sed iure impedit numquam! Beatae provident, ipsa corrupti odio quo
+      doloribus tempore soluta iste est, accusantium sint dolor quod maiores
+      numquam vel. Sapiente, labore officia? Itaque eum excepturi aut veritatis
+      sapiente perspiciatis reprehenderit fugiat nemo, sint vero cumque
+      repellendus! Magnam obcaecati, deserunt ex corrupti inventore molestiae
+      debitis velit reprehenderit eos.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,
+      quis dolores sunt saepe ea illum commodi vitae error molestias voluptas
+      porro nesciunt adipisci atque nemo tenetur sequi ad dicta deleniti
+      repellat ab animi? Praesentium quo rerum ipsa fugiat cumque, veniam natus
+      mollitia est consequatur ad odio dignissimos repellendus aliquid minima
+      sed iure impedit numquam! Beatae provident, ipsa corrupti odio quo
+      doloribus tempore soluta iste est, accusantium sint dolor quod maiores
+      numquam vel. Sapiente, labore officia? Itaque eum excepturi aut veritatis
+      sapiente perspiciatis reprehenderit fugiat nemo, sint vero cumque
+      repellendus! Magnam obcaecati, deserunt ex corrupti inventore molestiae
+      debitis velit reprehenderit eos.
+    </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi incidunt,
+      delectus, non architecto nam iure, dolor omnis necessitatibus voluptatibus
+      exercitationem officia autem. Quos minus debitis sit repellat voluptate
+      iure reprehenderit, omnis reiciendis dolorem eveniet beatae ipsum cumque
+      explicabo. Odit a maxime perspiciatis delectus, dolore nostrum facilis
+      tempora non recusandae consequuntur commodi aliquam ratione natus, cumque,
+      aliquid sit voluptate! Maiores quibusdam earum dicta repellendus maxime
+      corrupti exercitationem sit accusamus ex voluptatibus quisquam, voluptatem
+      unde, nisi, natus ratione quod reprehenderit iusto assumenda eligendi
+      saepe facilis! Numquam ipsum esse debitis natus id tempora similique sit
+      rerum veniam, minima quo quidem. Eaque, sunt ipsa.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi incidunt,
+      delectus, non architecto nam iure, dolor omnis necessitatibus voluptatibus
+      exercitationem officia autem. Quos minus debitis sit repellat voluptate
+      iure reprehenderit, omnis reiciendis dolorem eveniet beatae ipsum cumque
+      explicabo. Odit a maxime perspiciatis delectus, dolore nostrum facilis
+      tempora non recusandae consequuntur commodi aliquam ratione natus, cumque,
+      aliquid sit voluptate! Maiores quibusdam earum dicta repellendus maxime
+      corrupti exercitationem sit accusamus ex voluptatibus quisquam, voluptatem
+      unde, nisi, natus ratione quod reprehenderit iusto assumenda eligendi
+      saepe facilis! Numquam ipsum esse debitis natus id tempora similique sit
+      rerum veniam, minima quo quidem. Eaque, sunt ipsa.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
