e3d55f0c764c505cd1add4d853a0e73b2219c342
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-02-26 11:02:27 +0600

    Conditionally showing data

--- src/views/jobs/JobDetailsView.vue
+++ src/views/jobs/JobDetailsView.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>{{ job.title }}</h1>
-  <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading ...</p>
+  </div>
 </template>
 
 <script>
--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -1,9 +1,14 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="jobs">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="jobs">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading ...</p> 
   </div>
 </template>
 
