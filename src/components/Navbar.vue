1e44f17960566bf99d3a54aa27200cf59ad4aefd
Mehedi Hasan <email.mehedi.bd@gmail.com>
2023-08-21 20:08:15 +0600

    Progressing

---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+    <nav :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">My Vue</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li v-for="(page, index) in pages" class="nav-item" :key="index">
+                        <a class="nav-link" :class="{ active: activePage == index }" aria-current="page"
+                            :href="page.link.url" :title="`this link goes to ${page.link.text} page`"
+                            @click.prevent="activePage = index">
+                            {{ page.link.text }}</a>
+                    </li>
+                </ul>
+
+            </div>
+            <form class="d-flex">
+                <button class="btn btn-primary" @click.prevent="changeTheme()">
+                    Toggle Navbar
+                </button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<script>
+
+export default {
+    props: ['pages', 'activePage'],
+
+    data() {
+        return {
+            theme: 'light',
+        };
+    },
+    methods: {
+        changeTheme() {
+            let theme = 'light';
+
+            if (this.theme == 'light') {
+                theme = 'dark';
+            }
+
+            this.theme = theme;
+        }
+    },
+}
+
+</script>
---
+++ src/components/PageViewer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+
+        <h1>{{ page.pageTitle }}</h1>
+        <p>{{ page.content }}</p>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['page'],
+}
+
+</script>
